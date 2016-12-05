4d99a03070a70c0430f9cf96899c90a9edb3dafb
lianghaojiang88 <1129731598@qq.com>
2016-12-05 20:22:34 +0800

    jvm调优

---
+++ src/com/atguigu/jvm/TestJvm.java
@@ -0,0 +1,27 @@
+package com.atguigu.jvm;
+
+import java.util.Random;
+
+public class TestJvm {
+
+	public static void main(String[] args){
+		long maxMemory = Runtime.getRuntime().maxMemory() ;//返回 Java 虚拟机试图使用的最大内存量。
+		long totalMemory = Runtime.getRuntime().totalMemory() ;//返回 Java 虚拟机中的内存总量。
+		//String str = "jlh";
+		Object object = new Object();
+		System.out.println("MAX_MEMORY = " + maxMemory + "（字节）、" + (maxMemory / (double)1024 / 1024) + "MB");
+		System.out.println("TOTAL_MEMORY = " + totalMemory + "（字节）、" + (totalMemory / (double)1024 / 1024) + "MB");
+		//stack();
+		String str = "";
+	/*	while(true){
+			str += str + new Random().nextInt(88888888) + new Random().nextInt(999999999) ;
+
+		}*/
+	}
+
+	private static void stack() {
+		// TODO Auto-generated method stub
+		stack();
+	}
+
+}
